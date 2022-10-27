33366cb05569d7c06bd574cb12cdf0a61e51307f
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-27 18:23:08 -0400

    loads data into table

--- components/conference-table.jsx
+++ components/conference-table.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
 
 export default function ConferenceTable(props) {
+  const [confData, setConfData]= useState([])
+  
+  const getData = () => {
+    fetch('data.json', { 
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then(function (response) {
+      console.log(response)
+      return response.json()
+    }).then(function (myJson) {
+      setConfData(myJson)
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const TableContents = confData.map((conf) => {
+    return (
+      <div key={conf.id}>
+        <span>{conf.conference_name}</span>
+      </div>
+    )
+  })
+
   return (
-    <h1>Conference Table</h1>
+    // <h1>Conference Table</h1>
+    TableContents
   )
 }
